fe6b1367ccea92271b9f56da607167a3f53112a9
Mikhail Shkarubski <michaelshkarubski@gmail.com>
2023-11-09 13:13:31 +0300

    Lab. 8: UI updates for lab. No. 7

--- Lab78Client/Lab78Client.cpp
+++ Lab78Client/Lab78Client.cpp
@@ -1,19 +1,18 @@
-//
-// Created by Mikhail Shkarubski on 7.11.23.
-//
-
 #pragma comment(lib, "Ws2_32.lib")
 
 
 #include "Lab7Client.h"
 #include "ClientNetwork.h"
 #include <windows.h>
-
+#include <string>
 
 // Global variables for GUI controls
 HWND hwndInput;     // Text input field
 HWND hwndSendButton; // Send button
 HWND hwndMessageLog; // Message display area
+HWND hwndUsernameDisplay; // Username display area
+HWND hwndUsernameInput;  // Username input field
+HWND hwndConnectButton; // Connet to server button
 ClientNetwork clientNetwork;
 
 
@@ -46,12 +45,19 @@ DWORD WINAPI ClientReceiveThread(LPVOID param) {
 void OnSendButtonClick(HWND hWnd, ClientNetwork& clientNetwork) {
     wchar_t wMessage[256];
     char message[256];
+    char username[256];
 
     GetWindowTextA(hwndInput, message, sizeof(message));
-    GetWindowText(hwndInput, wMessage, sizeof(message));
+    GetWindowText(hwndInput, wMessage, sizeof(wMessage));
+    GetWindowTextA(hwndUsernameInput, username, sizeof(username));
+
+    // Create the message with the custom username prefix
+    std::string prefixedMessage = username;
+    prefixedMessage += ": ";
+    prefixedMessage += message;
 
     // Send the message to the server using clientNetwork
-    if (clientNetwork.Send((message))) {
+    if (clientNetwork.Send((prefixedMessage.c_str()))) {
         // Append the sent message to the message log
         SendMessage(hwndMessageLog, EM_SETSEL, -1, -1);
         SendMessage(hwndMessageLog, EM_REPLACESEL, 0, (LPARAM)wMessage);
@@ -65,6 +71,19 @@ void OnSendButtonClick(HWND hWnd, ClientNetwork& clientNetwork) {
     };
 }
 
+void OnConnectButtonClick(HWND hWnd) {
+    wchar_t wUsername[256];
+    char username[256];
+
+    GetWindowTextA(hwndUsernameInput, username, sizeof(username));
+    GetWindowText(hwndUsernameInput, wUsername, sizeof(username));
+
+    std::wstring usernameDisplay = L"Logged in as ";
+    usernameDisplay += wUsername;
+
+    SetWindowText(hwndUsernameDisplay, usernameDisplay.c_str());
+}
+
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
     switch (message) {
         case WM_CREATE:
@@ -76,9 +95,10 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) 
             if (lParam == (LPARAM)hwndSendButton && HIWORD(wParam) == BN_CLICKED)
                 OnSendButtonClick(hWnd, clientNetwork);
 
-            break;
+            if (lParam == (LPARAM)hwndConnectButton && HIWORD(wParam) == BN_CLICKED)
+                OnConnectButtonClick(hWnd);
 
-            // Handle other window messages here
+            break;
 
         default:
             return DefWindowProc(hWnd, message, wParam, lParam);
@@ -105,12 +125,26 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     wcex.lpszClassName = L"Lab7ClientWindowClass";
     RegisterClassEx(&wcex);
 
-    HWND hWnd = CreateWindow(L"Lab7ClientWindowClass", L"Lab7Client", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, 0, 450, 350, NULL, NULL, hInstance, NULL);
+    HWND hWnd = CreateWindow(
+        L"Lab7ClientWindowClass",
+        L"Lab7Client",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT,
+        800, 660, NULL, NULL,
+        hInstance, NULL
+    );
 
     // Create GUI controls
-    hwndInput = CreateWindow(L"EDIT", L"", WS_CHILD | WS_VISIBLE | WS_BORDER | ES_AUTOHSCROLL, 10, 10, 300, 30, hWnd, NULL, hInstance, NULL);
-    hwndSendButton = CreateWindow(L"BUTTON", L"Send", WS_CHILD | WS_VISIBLE | BS_DEFPUSHBUTTON, 320, 10, 80, 30, hWnd, NULL, hInstance, NULL);
-    hwndMessageLog = CreateWindow(L"EDIT", L"", WS_CHILD | WS_VISIBLE | WS_BORDER | ES_AUTOVSCROLL | ES_MULTILINE | ES_READONLY, 10, 50, 390, 240, hWnd, NULL, hInstance, NULL);
+    //hwndInput = CreateWindow(L"EDIT", L"", WS_CHILD | WS_VISIBLE | WS_BORDER | ES_AUTOHSCROLL, 10, 10, 300, 30, hWnd, NULL, hInstance, NULL);
+    //hwndSendButton = CreateWindow(L"BUTTON", L"Send", WS_CHILD | WS_VISIBLE | BS_DEFPUSHBUTTON, 320, 10, 80, 30, hWnd, NULL, hInstance, NULL);
+    //hwndMessageLog = CreateWindow(L"EDIT", L"", WS_CHILD | WS_VISIBLE | WS_BORDER | ES_AUTOVSCROLL | ES_MULTILINE | ES_READONLY, 10, 50, 390, 240, hWnd, NULL, hInstance, NULL);
+
+    hwndUsernameDisplay = CreateWindow(L"STATIC", L"", WS_CHILD | WS_VISIBLE, 10, 10, 390, 30, hWnd, NULL, NULL, NULL);
+    hwndUsernameInput = CreateWindow(L"EDIT", L"Your Username", WS_CHILD | WS_VISIBLE | WS_BORDER, 470, 50, 300, 30, hWnd, NULL, NULL, NULL);
+    hwndConnectButton = CreateWindow(L"BUTTON", L"Connect to server", WS_CHILD | WS_VISIBLE | BS_DEFPUSHBUTTON, 470, 90, 300, 30, hWnd, NULL, NULL, NULL);
+    hwndInput = CreateWindow(L"EDIT", L"", WS_CHILD | WS_VISIBLE | WS_BORDER | ES_AUTOHSCROLL, 10, 560, 300, 30, hWnd, NULL, NULL, NULL);
+    hwndSendButton = CreateWindow(L"BUTTON", L"Send", WS_CHILD | WS_VISIBLE | BS_DEFPUSHBUTTON, 320, 560, 80, 30, hWnd, NULL, NULL, NULL);
+    hwndMessageLog = CreateWindow(L"EDIT", L"", WS_CHILD | WS_VISIBLE | WS_BORDER | ES_AUTOVSCROLL | ES_MULTILINE | ES_READONLY, 10, 50, 390, 500, hWnd, NULL, NULL, NULL);
 
     // Show the main window
     ShowWindow(hWnd, nCmdShow);
@@ -125,4 +159,3 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
     return 0;
 }
-
